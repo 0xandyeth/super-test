dbf63fb52b6d47e1e26fec39a449fcab599e33fe
0xandyeth <andyjiang091222@gmail.com>
2023-10-19 03:46:55 -0500

    update readme

--- src/components/common/DropDown.tsx
+++ src/components/common/DropDown.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import Select from 'react-select';
 import { OptionType } from '../../constants/types';
 import axios from 'axios';
